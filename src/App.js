4fda4ac1e918674983c6a0b70166dca751f40b59
k.kitajima <k.kitajima@conol.jp>
2019-11-26 11:48:40 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import Rect from './Rect';
+import './test.css';
+import './reset.css';
+
+
+//仮のデータ
+let store_data = { //コンテキストに入れるものはもっと多重配列にできるのか。
+  store_name: 'モスバーガー',
+  store_place: '西口11番店舗',
+  order1: "ハンバーガー",
+  order2: "ポテト",
+  total: "220",
+  order_time: "12:00:00"
+};
+
+
+
+//コンテキスト
+// const data = React.createContext(store_data); //ここを2つに出来ないか。
+
+
+class App extends Component {
+  
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) =>{
+        this.setState({
+          movies: responseJson.movies,
+          title: responseJson.title
+        })
+
+        // console.log(data);
+        this.state.title=responseJson.title;
+        this.state.store_name = responseJson.title;
+      }
+      )
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+  render() {
+    return (
+      <div>
+          <Head />
+          <Header />
+          <Body />
+
+          <Footer />
+      </div>
+    );
+  }
+}
+
+
+class Head extends Component {
+  render() {
+    return (
+    <div>
+      <html lang="ja" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="/Users/Kento/Desktop/React超入門/chapter/react_app/static/css/test.css" />
+        <link rel="stylesheet" href="/Users/Kento/Desktop/React超入門/chapter/react_app/static/css/reset.css" />
+        {/* <title>UberEats画面</title> */}
+    </div>
+    );
+  }
+}
+
+class Header extends Component {
+  // static contextType = data;
+
+  render() {
+    return(
+      <header>
+        <div>
+          <p class="left"><a href="#">←</a></p>
+          <p>{this.context.store_name}</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+class Body extends Component {
+  // static contextType = data;
+
+  render() {
+    return (
+      <div>
+
+        <main>
+          <div className="content">
+            <section className="store-name">
+              <h1>{this.context.store_name}</h1>
+              <p>{this.context.store_place}</p>
+            </section>
+
+            <section className="map">
+              {this.state.title}
+              <h1>なにか説明</h1>
+            </section>
+
+            <section className="detail">
+              <div className="detail1">
+                <p>● </p>
+              </div>
+              <div className="detail1">
+                <p>● Order Time: {this.context.order_time}</p>
+              </div>
+            </section>
+
+            <section className="order">
+              <div className="your-order">
+                <h1>Your order</h1>
+                <p>price</p>
+              </div>
+              <div className="orders">
+                <p>①ハンバーガー</p>
+                <p>100円</p>
+              </div>
+              <div className="orders">
+                <p>②ポテト</p>
+                <p>120円</p>
+              </div>
+            </section>
+
+            </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+
+class Footer extends Component {
+
+  // static contextType = data;
+
+  render() {
+    return(
+      <footer>
+        <div>
+          <h1><a href="#">確認画面へ</a></h1>
+          <p class="right">{this.context.total}円</p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// index.html → index.js → App.js の順で読み込まれ表示される 最終的にApp.jsの内容が画面に描画されると思って良い
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <App msg="Hello App"/>, document.getElementById('root')
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
